1f175023b4630cabe490acf9d21f2895b35bd596
James B Schoen <edensongjbs@gmail.com>
2020-05-11 15:00:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,7 +159,7 @@ def team_colors(team_name)
   game_hash.reduce(nil){ |memo, (away_or_home, team)|
     if team[:team_name]==team_name
       return team[:colors]
-    }
+    end
     memo
   }
   return nil
