9329d4e2fc95d81d9af78785a8667b650b8ecc4d
James B Schoen <edensongjbs@gmail.com>
2020-05-11 14:51:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,9 +131,7 @@ end
 # Write code here
 def num_points_scored(player_name)
   game_hash.reduce(nil){ |m1, (away_or_home, team)|
-    #binding.pry
     team[:players].reduce(m1) { |m2, player|
-      #binding.pry
       if player[:player_name]==player_name
         return player[:points]
       end
